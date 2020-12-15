74ad6c9982552465274998878f118be900d53db8
Augusto Kono <augustokono@gmail.com>
2020-12-15 20:06:58 -0300

    DrawerUpdated to route

--- resources/js/components/DrawerMenu.js
+++ resources/js/components/DrawerMenu.js
@@ -9,27 +9,17 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import IconButton from '@material-ui/core/IconButton';
-import HistoryIcon from '@material-ui/icons/History';
-import ListIcon from '@material-ui/icons/List';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 import useStyles from './styles'
 
 function DrawerMenu(buttonProps) {
 
+
 	const classes = useStyles(theme);
 	const theme = useTheme();
 
-	const {open, handleDrawer} = buttonProps
-
-	const options = [{
-		text: "Item Storage",
-		icon: (<ListIcon/>)
-	},
-	{
-		text: "Item History",
-		icon: (<HistoryIcon/>)
-	}]
+	const {open, handleDrawer, DrawerOptions} = buttonProps
 
 	return (
 		<div>
@@ -53,8 +43,8 @@ function DrawerMenu(buttonProps) {
 				</div>
 				<Divider />
 				<List>
-					{options.map((item, index) => (
-						<ListItem button key={index}>
+					{DrawerOptions.map((item, index) => (
+						<ListItem button key={index} onClick={item.action} >
 							<ListItemIcon>{item.icon}</ListItemIcon>
 							<ListItemText primary={item.text} />
 						</ListItem>
--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './Header'
+import HistoryIcon from '@material-ui/icons/History';
+import ListIcon from '@material-ui/icons/List';
 import DrawerMenu from './DrawerMenu'
+import ProductList from './ProductList'
+import HistoryPage from './HistoryPage'
 
 import useStyles from './styles'
 
@@ -10,6 +14,34 @@ function Main() {
     const classes = useStyles();
 
     const [open, setOpen] = useState(false);
+    const [route, setRoute] = useState(false);
+
+    const options = [{
+		text: "Product Storage",
+        icon: (<ListIcon/>),
+        action: () => setRoute("Product Storage")
+	},
+	{
+		text: "Product History",
+        icon: (<HistoryIcon/>),
+        action: () => setRoute("Product History")
+    }]
+    
+
+    const router = () => {
+        switch (route) {
+            case "Product Storage":
+                return (<ProductList />)
+                break;
+
+            case "Product History" :
+                return (<HistoryPage />)
+                break;
+            default:
+                return (<ProductList />)
+                break;
+        }
+    } 
 
     const handleDrawer = () => {
         if (open) {
@@ -19,6 +51,7 @@ function Main() {
         }
     };
 
+
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -27,9 +60,14 @@ function Main() {
                 open
             }} />
             <DrawerMenu {...{
+                DrawerOptions: options,
                 handleDrawer: handleDrawer,
                 open
-            }}  />
+            }} />
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {router()}
+            </main>
         </div>
     );
 }
